6fe7c0422ab7598393d66e14f5c19dfa1e8cc8c2
gabrielotsuka <gabrielotsuka@gmail.com>
2022-01-19 09:33:45 -0300

    WIP HTTPServer

--- HTTPServer.py
+++ HTTPServer.py
@@ -1,35 +1,38 @@
+from logging import exception
 import socket
 
 # PORT < 1024 can require superuser permissions.
 HOST, PORT = '127.0.0.1', 8080
 
-def getFileName(requestData):
-    refererIndex = requestData.find("Ref")
-    if refererIndex != -1:
-        referer = requestData[refererIndex:requestData.find("Accept-Encoding")]
-        fileName = referer[referer.find("0/")+2:-2]
-        return fileName
-        
-if __name__ == "__main__":
-    # TCP connection configuration. AF_INET refers to ipv4 and SOCK_STREAM is the TCP connection
-    listenSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    listenSocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    listenSocket.bind((HOST, PORT))
-    listenSocket.listen(1)
-    print('Serving HTTP on port {PORT}...')
-
-    while True:    
-        clientConnection, clientAddress = listenSocket.accept()
-        with clientConnection: # As it uses with statement, it's not necessary to call clientConnection.close()
-            requestData = clientConnection.recv(1024).decode('utf-8')
-            fileName = getFileName(requestData)
-            if fileName != None:
-                print(fileName)
-            
-
-            http_response = b"""\
+def buildResponse():
+    return b"""\
 HTTP/1.1 200 OK
 
 Hello world
             """
-            clientConnection.sendall(http_response)
+
+if __name__ == "__main__":
+    # TCP connection configuration. AF_INET refers to ipv4 and SOCK_STREAM is the TCP connection
+    listenSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    listenSocket.bind((HOST, PORT))
+    listenSocket.listen(1)
+    print('Serving HTTP on port ' + str(PORT))
+
+    while True:    
+        clientConnection, clientAddress = listenSocket.accept()
+        with clientConnection: # As it uses with statement, it's not necessary to call clientConnection.close()
+            requestData = clientConnection.recv(4096).decode('utf-8')
+            fileName = requestData[5:requestData.find("HTTP/1.1")-1]
+            if fileName != 'favicon.ico':
+                if fileName == '':
+                    print('Invalid Path URL. Please insert a path in the following format: localhost:' + str(PORT) + '/<file_name.ext>')
+                    clientConnection.sendall(buildResponse())
+                    continue
+                try:
+                    with open(fileName, 'rb') as f:
+                        file = f.read()
+                        print('cheguei aqui')
+                except FileNotFoundError:
+                    print('File ' + fileName + ' not found')
+
+            clientConnection.sendall(buildResponse())
